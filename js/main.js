3f517ecefb511b8c981e639f50238add982e979b
jungseungtae <76475302+jungseungtae@users.noreply.github.com>
2021-07-05 14:00:44 +0900

    0705

--- js/main.js
+++ js/main.js
@@ -18,3 +18,34 @@ searchInputEl.addEventListener('blur', function () {
   searchEl.classList.remove('focused')
   searchInputEl.setAttribute('placeholder', '')
 })
+
+const badgeEl = document.querySelector('header .badges');
+
+// _.throttle(함수, 실행간격 시간)
+window.addEventListener('scroll', _.throttle(function() {
+  console.log(window.scrollY);
+  if (window.scrollY > 500) {
+  //  badgeEl.style.display = 'none';
+    // gsap.to(요소, 지속시간, 옵션);
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    });
+  } else {
+    // badgeEl.style.display = 'block';
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    });
+  }
+}, 1000));
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+
+fadeEls.forEach(function (fadeEl, index) {
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * .7, // 순차적으로 작동하도록 시간을 지정
+    opacity: 1
+  });
+});
+
